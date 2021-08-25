32f564bcd430f1b5960b4e2de3f10a6a00f8d916
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-08-25 17:36:01 +0200

    Small changes for debugging

--- sim/bulb3dtest.py
+++ sim/bulb3dtest.py
@@ -6,8 +6,8 @@ import odors
 from math import sqrt
 
 params.filename = 'bulb3dtest'
-params.tstop = 1050
-params.coreneuron = False
+params.tstop = 10
+params.coreneuron = True 
 params.gpu = False
 params.sniff_invl_min = params.sniff_invl_max = 500
 params.training_exc = params.training_inh = True
@@ -16,5 +16,5 @@ h('sigslope_AmpaNmda=5')
 h('sigslope_FastInhib=5')
 h('sigexp_AmpaNmda=4')
 params.odor_sequence = [ ('Onion', 50, 1000, 1e+9) ]
-runsim.build_part_model([5,37,32,78,7], [])
+runsim.build_part_model([7], [])
 runsim.run()
